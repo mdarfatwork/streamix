e24b9a9074f91b09ed9ba0b0faef93c944947fc9
unknown <work.arfat@gmail.com>
2024-08-26 11:45:27 +0530

    page animation add

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,14 @@
+"use client"
 import React from 'react'
-import { currentUser } from '@clerk/nextjs/server'
+import { useUser, useClerk } from '@clerk/nextjs'
 
-const Page = async () => {
-  const user = await currentUser()
+const Page = () => {
+  const { user } = useUser()
+  const { signOut } = useClerk()
   return (
     <section className='min-h-screen'>
       <div>Hello {user?.firstName} {user?.lastName}</div>
+      <button onClick={() => signOut({ redirectUrl: '/' })} >Sign Out</button>
     </section>
   )
 }
--- components/home/ShowMovies.tsx
+++ components/home/ShowMovies.tsx
@@ -5,6 +5,7 @@ import { RxCross1 } from 'react-icons/rx'
 import Image from 'next/image';
 import Link from 'next/link';
 import debounce from "lodash.debounce";
+import { motion } from 'framer-motion';
 
 type Props = {
     countryCode: string | undefined;
@@ -20,11 +21,9 @@ interface Movie {
 
 const ShowMovies: React.FC<Props> = ({ countryCode, onSend }) => {
     const [movies, setMovies] = useState<Movie[]>([]);
-    const [loading, setLoading] = useState<boolean>();
 
     const fetchMovies = debounce(async () => {
         try {
-            setLoading(true);
             const responseMovies = await axios.get(`https://api.themoviedb.org/3/discover/movie?include_adult=false&page=1&sort_by=popularity.desc&with_origin_country=${countryCode}`, {
                 headers: {
                     accept: 'application/json',
@@ -34,8 +33,6 @@ const ShowMovies: React.FC<Props> = ({ countryCode, onSend }) => {
             setMovies(responseMovies.data.results);
         } catch (error) {
             console.error('Error fetching movies:', error);
-        } finally {
-            setLoading(false);
         }
     }, 300);
 
@@ -43,36 +40,34 @@ const ShowMovies: React.FC<Props> = ({ countryCode, onSend }) => {
         if (countryCode) fetchMovies();
     }, [countryCode]);
 
+    if (movies.length === 0) return <div className="h-96 sm:h-[400px] md:h-[450px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] rounded-md bg-gradient-to-r from-gray-100 to-gray-300 animate-pulse"></div>;
+
     return (
-        <div className="w-full px-4 bg-blue-50 z-10 pb-2">
-            {loading ? (
-                <div className="h-96 sm:h-[400px] md:h-[450px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] rounded-md bg-gradient-to-r from-gray-100 to-gray-300 animate-pulse"></div>
-            ) : (
-                <div className="bg-blue-500 rounded-md p-3 min-h-96 sm:min-h-[400px] md:min-h-[450px] lg:min-h-[500px] xl:min-h-[600px] 2xl:min-h-[700px] relative">
-                    <span onClick={() => onSend(false)} className='absolute top-2 md:top-3 xl:top-4 right-2 md:right-3 xl:right-4 z-10 p-3 bg-blue-50 text-blue-500 rounded-full cursor-pointer'><RxCross1 /></span>
-                    <div className="grid grid-cols-1 gap-4 md:gap-6 xl:gap-8 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 p-2 md:p-3 xl:p-4">
-                        {movies.length > 0 ? (
-                            movies.filter(movie => movie.poster_path).map((movie) => (
-                                <div key={movie.id} className="relative cursor-pointer">
-                                    <Link href="/sign-in">
-                                        <Image
-                                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                            width={500}
-                                            height={500}
-                                            alt={movie.title}
-                                            className="w-full h-[90%] object-cover rounded-lg"
-                                        />
-                                        <p className='py-2 text-center text-blue-50'>{movie.title} ({new Date(movie.release_date).getFullYear()})</p>
-                                    </Link>
-                                </div>
-                            ))
-                        ) : (
-                            <span className='py-2 text-center text-xl'>No movies to Show from this country</span>
-                        )}
-                    </div>
+        <section className="w-full px-4 bg-blue-50 z-10 pb-2">
+            <motion.div className="bg-blue-500 rounded-md p-3 min-h-96 sm:min-h-[400px] md:min-h-[450px] lg:min-h-[500px] xl:min-h-[600px] 2xl:min-h-[700px] relative" animate={{ y: [100, 50, 0] }} transition={{ ease: "easeOut", duration: 0.5 }} >
+                <span onClick={() => onSend(false)} className='absolute top-2 md:top-3 xl:top-4 right-2 md:right-3 xl:right-4 z-10 p-3 bg-blue-50 text-blue-500 rounded-full cursor-pointer'><RxCross1 /></span>
+                <div className="grid grid-cols-1 gap-4 md:gap-6 xl:gap-8 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 p-2 md:p-3 xl:p-4">
+                    {movies.length > 0 ? (
+                        movies.filter(movie => movie.poster_path).map((movie) => (
+                            <div key={movie.id} className="relative cursor-pointer">
+                                <Link href="/sign-in">
+                                    <Image
+                                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                        width={500}
+                                        height={500}
+                                        alt={movie.title}
+                                        className="w-full h-[90%] object-cover rounded-lg"
+                                    />
+                                    <p className='py-2 text-center text-blue-50'>{movie.title} ({new Date(movie.release_date).getFullYear()})</p>
+                                </Link>
+                            </div>
+                        ))
+                    ) : (
+                        <span className='py-2 text-center text-xl'>No movies to Show from this country</span>
+                    )}
                 </div>
-            )}
-        </div>
+            </motion.div>
+        </section>
     )
 }
 
